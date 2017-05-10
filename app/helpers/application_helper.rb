7caa068eb7ca08c708015f4780f140b8a0de71ec
h8windows <h8windows@gmail.com>
2017-05-10 04:52:42 -0700

    Add title functionality for project show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def title(*parts)
+        unless parts.empty?
+            content_for :title do
+                (parts << "Tickettracker").join(" - ")
+            end
+        end
+    end
 end
